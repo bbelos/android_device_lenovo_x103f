c2b1a668d0d50326ad112b835e8579528b38fa41
Brian Beloshapka <bbelos@hotmail.com>
2017-02-11 19:16:09 -0600

    Create wlan symlink
    Change-Id: Id28714587ffe594ead9e9870085032cbbb7544c3

--- Android.mk
+++ Android.mk
@@ -7,13 +7,39 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),x103f)
-#LOCAL_MODULE       := wpa_supplicant.conf
-#LOCAL_MODULE_TAGS  := optional
-#LOCAL_MODULE_CLASS := ETC
-#LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-#LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
-#include $(BUILD_PREBUILT)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+WCNSS_CFG_INI := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+$(WCNSS_CFG_INI): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS_qcom_cfg.ini Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /data/misc/wifi/$(notdir $@) $@
+
+PERSIST_DICT := $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
+$(PERSIST_DICT): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS_wlan_dictionary.dat Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /persist/$(notdir $@) $@
+
+PERSIST_MAC := $(TARGET_OUT_ETC)/firmware/wlan/prima/wlan_mac.bin
+$(PERSIST_MAC): $(LOCAL_INSTALLED_MODULE)
+	@echo "wlan_mac.bin Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /persist/$(notdir $@) $@
+
+WLAN_MODULE_SYMLINK := $(TARGET_OUT)/lib/modules/wlan.ko
+$(WLAN_MODULE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "wlan.ko module link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib/modules/pronto/pronto_wlan.ko $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_CFG_INI) $(PERSIST_DICT) $(PERSIST_MAC) $(WLAN_MODULE_SYMLINK)
 
 endif
