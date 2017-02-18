118ef2a6d6697357855d213ae6a35fe7354ea66d
Brian Beloshapka <bbelos@hotmail.com>
2017-02-18 17:50:35 -0600

    Add BOARD_SUPPORTS_SOUND_TRIGGER
    Change-Id: I41af562f49fd3831e03928204f6ce7da48ad2ec7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 
 BOARD_USES_ALSA_AUDIO := true
+BOARD_SUPPORTS_SOUND_TRIGGER := true
 USE_CAMERA_STUB := true
 
 -include $(QCPATH)/common/msm8909/BoardConfigVendor.mk
